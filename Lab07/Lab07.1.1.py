48677088d27da9e165d8ab037cb00316800541e5
M. Aghavali <Aghavali9@gmail.com>
2022-11-21 19:20:24 +0330

    Update Lab07.1.1.py
    [07.1.1] Sum with alternating signs. Write a program that receives as input a sequence of integers (terminated by an empty line), and that calculates the alternating sum of its elements. For example, if the program reads the data 1 4 9 16 9 7 4 9 11, it must calculate and display 1 - 4 + 9 - 16 + 9 - 7 + 4 - 9 + 11 = –2.

--- Lab07/Lab07.1.1.py
+++ Lab07/Lab07.1.1.py
@@ -1,1 +1,36 @@
 
+def main():
+
+    numbers = get_numbers()
+    print(f"weird sum is = {weird_sum(numbers)}")
+
+
+def get_numbers():
+    user_input = input("Please enter int:\n\t>>> ")
+    list_ints = []
+    while user_input:
+        if not validator(user_input):
+            user_input = input("Please enter valid int:\n\t>>> ")
+        else:
+            list_ints.append(int(user_input))
+            user_input = input("Please enter another int:\n\t>>> ")
+    return list_ints
+
+
+def validator(x):
+    return x.isdigit()
+
+
+def weird_sum(raw_list):
+    modified_list = []
+    for i, e in enumerate(raw_list):
+        if i % 2 != 0:
+            modified_list.append(e * -1)
+        else:
+            modified_list.append(e)
+    total = sum(modified_list)
+    return total
+
+
+if __name__ == '__main__':
+    main()
