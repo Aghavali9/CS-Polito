5060a367d1041f424b7e206d1cb01f947a913f5c
M. Aghavali <Aghavali9@gmail.com>
2022-12-10 22:01:26 +0100

    Add files via upload

---
+++ Lab10/Lab10.1.5.py
@@ -0,0 +1,29 @@
+"""[1.5] Second possibility.
+Write a program that asks the user to enter a series of values of type float.
+When the user enters a value that is not of the correct type, the program must give the user a second chance to enter the value,
+and after two chances it must stop reading the input, terminating the program immediately.
+Data entry continues until the user enters an empty string ('') as input.
+Sum all correctly specified values and display their sum when the user has finished entering data.
+Use exception handling to detect improper input."""
+
+# Lab10.1.5
+
+def main():
+    flag_1 = False
+    chances = 2
+    floats = []
+    while (not flag_1) and chances != 0:
+        try:
+            input_ = input(f'Enter floats, (empty to end).\n\t{chances} {"chance" if chances == 1 else "chances"} remaining\n\t>>> ')
+            float_ = float(input_)
+        except ValueError:
+            print(f'{"Please enter a valid float number" if chances > 1 else "GAME OVER"}')
+            chances -= 1
+        else:
+            floats.append(float_)
+    print(f'Sum of the correctly entered values is: {sum(floats)}')
+
+
+
+if __name__ == '__main__':
+    main()
